dbefd16319e1702fbfbf47b95f46415e264839e3
Sujan <120612188+sujan-22@users.noreply.github.com>
2023-09-27 16:35:34 -0400

    final changes

--- Lab1/Employee.cs
+++ Lab1/Employee.cs
@@ -1,4 +1,4 @@
-﻿//I, Sujan Rokad, 000882948 certify that this material is my origianl work. No other person's work has been used without due acknowledgement.
+//I, Sujan Rokad, 000882948 certify that this material is my origianl work. No other person's work has been used without due acknowledgement.
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,11 +9,11 @@ namespace Lab1
 {
     internal class Employee
     {
-        // Private fields to store employee information
-        private string name;
-        private int number;
-        private decimal rate;
-        private double hours;
+        // instance variables to store employee information
+        private string name; // Employee name
+        private int number; // Employee number
+        private decimal rate; // Employee pay rate
+        private double hours; // Employee hours
 
         // Constructor to initialize employee information
         public Employee(string name, int number, decimal rate, double hours)
@@ -24,7 +24,7 @@ namespace Lab1
             this.hours = hours;
         }
 
-        // Getter methods
+        // GetGross method to calculate gross pay
         public decimal GetGross()
         {
             // Calculate gross pay with overtime
@@ -40,51 +40,58 @@ namespace Lab1
             }
         }
 
+        // GetHours method to retrieve the hours worked by the employee
         public double GetHours()
         {
             return hours;
         }
 
+        // GetName method to retrieve the name of the employee
         public string GetName()
         {
             return name;
         }
 
+        // GetNumber method to retrieve the employee number
         public int GetNumber()
         {
             return number;
         }
 
+        // GetRate method to retrieve the pay rate of the employee
         public decimal GetRate()
         {
             return rate;
         }
 
-        // Setter methods
+        // SetHours method to update the hours worked by the employee
         public void SetHours(double hours)
         {
             this.hours = hours;
         }
 
+        // SetName method to update the name of the employee
         public void Setname(string name)
         {
             this.name = name;
         }
 
+        // SetNumber method to update the employee number
         public void SetNumber(int number)
         {
             this.number = number;
         }
 
+        // SetRate method to update the pay rate of the employee
         public void SetRate(decimal rate)
         {
             this.rate = rate;
         }
-        
-        // ToString method to format employee information
+
+        // ToString method to format the employee information as a string
         public override string ToString()
         {
-            return $"{name ,-21} {number,-(int)9.5} {rate,-(int)9.5:C} {hours,-8:F2} {GetGross(),-(int)9.5:C}";
+            return $"{name,-21} {number,-(int)9.5} {rate,-(int)9.5:C} {hours,-8:F2} {GetGross(),-(int)9.5:C}";
         }
     }
 
